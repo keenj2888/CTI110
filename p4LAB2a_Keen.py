7415866f858681ab1926d8702350be0b604963cd
keenj2888 <107164156+keenj2888@users.noreply.github.com>
2022-07-10 15:55:45 -0400

    Add files via upload

---
+++ p4LAB2a_Keen.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.shape("turtle")
+
+
+for i in [0, 1, 2, 3]:
+    turtle.forward(100)
+    turtle.left(90)
+
+for i in [0,1,2,3]:
+    
+    turtle .forward(150)
+    turtle.left(120)
+
+
+
+
+
+
